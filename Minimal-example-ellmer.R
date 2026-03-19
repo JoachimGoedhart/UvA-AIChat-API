b4968148499fec65cb77fb09f259dab894ce9727
Joachim Goedhart <39348589+JoachimGoedhart@users.noreply.github.com>
2026-03-19 22:47:13 +0100

    Updated with new endpoint

--- Minimal-example-ellmer.R
+++ Minimal-example-ellmer.R
@@ -1,20 +1,19 @@
 # Minimal example that shows how to use the UvA Chat API with {ellmer}
 # In the .Renviron file set these variables:
-#
-# OPENAI_BASE_URL = https://ai-research-proxy.azurewebsites.net/
 #  
 # OPENAI_API_KEY = "sk-replace_with_actual_APIkey"
 
+library(httr2)
 library(tidyverse)
 library(ellmer)
 
 api_key <- Sys.getenv("OPENAI_API_KEY")
-base_url <- Sys.getenv("OPENAI_BASE_URL")
+base_url = "https://llmproxy.uva.nl"
 
 chat <- chat_openai_compatible(
   system_prompt = "be terse",
   base_url = base_url,
-  model = "gpt-4.1"
+  model = "gpt-5.1"
 )
 
 chat$chat("What is the best place to store an API key when working with R?")
